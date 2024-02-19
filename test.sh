761b954b2230988a4e0a812699cec284084b0d70
Zvonimir Delas <zvonimird@dump.hr>
2024-02-19 18:49:07 +0000

    secrets

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+secrets="$SECRETS_STAGING"
+
+[[ -z "$SECRETS" ]] && { echo "No secrets specified, skipping step" ; exit 0; }
+
+mkdir -p .secrets && cd "$_"
+
+i=1
+while secret=$(echo "$SECRETS" | cut -d "&" -f $i) ; [ -n "$secret" ] ;do
+    i=$((i+1))
+
+    key=$(echo "$secret" | cut -d "=" -f 1)
+    value=$(echo "$secret" | cut -d "=" -f 2)
+
+    echo "$value" > "$key"
+done
